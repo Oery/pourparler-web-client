236fa0cb963c8280d18cd6d50d4a90db68235e6c
Oery <48154940+Oery@users.noreply.github.com>
2024-05-08 01:12:26 +0200

    Changed Main Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "~/styles/globals.css";
 
-import { Inter } from "next/font/google";
+import { Lexend } from "next/font/google";
 
-const inter = Inter({
+const lexend = Lexend({
     subsets: ["latin"],
     variable: "--font-sans",
 });
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={`font-sans ${inter.variable}`}>{children}</body>
+            <body className={`font-sans ${lexend.variable}`}>{children}</body>
         </html>
     );
 }
