4dc11b1287819422e64de1b965dc8403f4b5a8a4
Oery <48154940+Oery@users.noreply.github.com>
2024-05-08 00:04:38 +0200

    Add Icons to Channel Component

--- src/app/[channel_id]/_components/channel.tsx
+++ src/app/[channel_id]/_components/channel.tsx
@@ -1,3 +1,6 @@
+import HashIcon from "./icon-hash";
+import MicIcon from "./icon-mic";
+
 interface Props {
     channel: {
         id: number;
@@ -9,6 +12,11 @@ interface Props {
 export default function Channel({ channel }: Props) {
     return (
         <div className="flex cursor-pointer items-center gap-3 truncate rounded-md px-4 py-1 text-base font-light transition-all hover:translate-x-2 hover:bg-stone-200">
+            {channel.type === "voice" ? (
+                <MicIcon className="inline-block h-4 min-h-4 w-4 min-w-4" />
+            ) : (
+                <HashIcon className="inline-block h-4 min-h-4 w-4 min-w-4" />
+            )}
             {channel.name}
         </div>
     );
