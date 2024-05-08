dd94823189e9faf5950974443d25b5779b37c085
Oery <48154940+Oery@users.noreply.github.com>
2024-05-08 22:04:34 +0200

    Add Chat Message component

---
+++ src/app/[channel_id]/_components/chat-message.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+import { type MessageWithAuthor } from "./definitions";
+
+interface Props {
+    message: MessageWithAuthor;
+}
+
+export default function ChatMessage({ message }: Props) {
+    return (
+        <div className="flex flex-row gap-4 first:mt-auto">
+            <aside>
+                <img
+                    className="h-10 min-w-10 rounded-full"
+                    src={message.author.avatarUrl}
+                    alt={message.author.name}
+                />
+            </aside>
+            <div>
+                <div className="flex flex-row items-center gap-1 font-semibold text-red-600">
+                    {message.author.name}
+                    <span className="align-baseline text-xs font-normal leading-5 text-gray-400">
+                        {message.createdAt.toLocaleTimeString()}
+                    </span>
+                </div>
+                <div className="overflow-hidden text-base text-stone-600">
+                    {message.content}
+                </div>
+            </div>
+        </div>
+    );
+}
