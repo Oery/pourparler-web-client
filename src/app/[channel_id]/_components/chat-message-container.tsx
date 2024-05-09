769a6c754a847c04af1650fe6451f5a994e9f889
Oery <48154940+Oery@users.noreply.github.com>
2024-05-09 15:52:29 +0200

    Add Auto Scroll to last Message

--- src/app/[channel_id]/_components/chat-message-container.tsx
+++ src/app/[channel_id]/_components/chat-message-container.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import ChatMessage from "./chat-message";
 import { type MessageWithAuthor } from "./definitions";
 
@@ -8,11 +9,23 @@ interface Props {
 }
 
 export default function ChatMessageContainer({ messages }: Props) {
+    const lastMessageRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
+
     return (
-        <div className="scrollbar-hide flex grow basis-px flex-col gap-4 overflow-y-scroll p-5">
-            {messages.map((message) => (
-                <ChatMessage key={message.id} message={message} />
-            ))}
+        <div className="scrollbar-hide flex grow basis-px flex-col overflow-y-scroll p-5 pb-7">
+            <div className="flex flex-col gap-4">
+                <div className="h-screen" />{" "}
+                {/* This is a hack to make the last message scroll into view */}
+                {messages.map((message) => (
+                    <ChatMessage key={message.id} message={message} />
+                ))}
+            </div>
+            <div ref={lastMessageRef} />{" "}
+            {/* This is a hack to make the last message scroll into view */}
         </div>
     );
 }
