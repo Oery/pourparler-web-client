218a05855e15c2e3af4860d211eaf3307943dcb1
Oery <48154940+Oery@users.noreply.github.com>
2024-05-09 18:19:18 +0200

    Add Better layout for messages

--- src/app/[channel_id]/_components/chat-message-container.tsx
+++ src/app/[channel_id]/_components/chat-message-container.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import ChatMessage from "./chat-message";
 import { type MessageWithAuthor } from "./definitions";
 
@@ -10,21 +10,44 @@ interface Props {
 
 export default function ChatMessageContainer({ messages }: Props) {
     const lastMessageRef = useRef<HTMLDivElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [height, setHeight] = useState(0);
 
     useEffect(() => {
+        const updateHeight = () => {
+            const container = containerRef.current;
+            if (!container) return;
+
+            const messageContainer = container.children[1];
+            if (!messageContainer) return;
+
+            setHeight(messageContainer.clientHeight);
+        };
+
+        updateHeight();
+        window.addEventListener("resize", updateHeight);
+
         lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+
+        return () => {
+            window.removeEventListener("resize", updateHeight);
+        };
     }, [messages]);
 
     return (
-        <div className="scrollbar-hide flex grow basis-px flex-col overflow-y-scroll p-5 pb-7">
-            <div className="flex flex-col gap-4">
-                <div className="h-screen" />{" "}
+        <div className="scrollbar-hide relative flex grow basis-px flex-col overflow-y-scroll p-5 pb-7">
+            <div
+                style={{
+                    minHeight: `calc(100% - ${height}px)`,
+                }}
+            />
+            <div className="flex flex-col gap-4" ref={containerRef}>
                 {/* This is a hack to make the last message scroll into view */}
                 {messages.map((message) => (
                     <ChatMessage key={message.id} message={message} />
                 ))}
             </div>
-            <div ref={lastMessageRef} />{" "}
+            <div ref={lastMessageRef} />
             {/* This is a hack to make the last message scroll into view */}
         </div>
     );
