32c0f9715c05465472db426d0e479462ab5d22c6
Oery <48154940+Oery@users.noreply.github.com>
2024-05-09 13:36:35 +0200

    Add Socket hook

---
+++ src/app/[channel_id]/_hooks/use-socket.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { createContext, useContext, useMemo } from "react";
+import { io, type Socket } from "socket.io-client";
+
+const SocketContext = createContext<Socket | null>(null);
+
+export const useSocket = () => {
+    const socket = useContext(SocketContext);
+    return socket;
+};
+
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+    const socket = useMemo(() => io("localhost:8000"), []);
+
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
