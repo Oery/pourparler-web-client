d3c7b10ff45a989fb5f698d11fb3f3a9ccaf5fd6
Oery <48154940+Oery@users.noreply.github.com>
2024-05-09 13:56:16 +0200

    Add WS Chat Reception

--- src/app/[channel_id]/_components/chat.tsx
+++ src/app/[channel_id]/_components/chat.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { useSocket } from "../_hooks/use-socket";
 import ChatInput from "./chat-input";
 import ChatMessageContainer from "./chat-message-container";
 import type { MessageWithAuthor, ChannelWithMessages } from "./definitions";
+import { useCallback, useEffect, useState } from "react";
 
 interface Props {
     channel: ChannelWithMessages;
@@ -12,6 +14,20 @@ export default function Chat({ channel }: Props) {
     const [messages, setMessages] = useState<MessageWithAuthor[]>(
         channel.messages,
     );
+    const socket = useSocket();
+
+    const handleMessageReception = useCallback((message: MessageWithAuthor) => {
+        setMessages((prev) => [...prev, message]);
+    }, []);
+
+    useEffect(() => {
+        if (!socket) return;
+        socket.on("message:send", handleMessageReception);
+
+        return () => {
+            socket.off("message:send");
+        };
+    }, [handleMessageReception, socket]);
 
     return (
         <div className="flex grow flex-col">
