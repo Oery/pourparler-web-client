60fef9b45eb76354e02840ae5c0a89ad45e9c02a
Oery <48154940+Oery@users.noreply.github.com>
2024-05-11 20:31:10 +0200

    Moved Auth one layer up

--- src/app/[server_id]/[channel_id]/layout.tsx
+++ src/app/[server_id]/[channel_id]/layout.tsx
@@ -1,4 +1,3 @@
-import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import SideNav from "./_components/sidenav";
 
 interface Props {
@@ -7,22 +6,13 @@ interface Props {
 
 export default function ChannelLayout({ children }: Props) {
     return (
-        <>
-            <SignedOut>
-                <h1>You are not logged in</h1>
-                <p>Please log in to continue</p>
-                <SignInButton />
-            </SignedOut>
-            <SignedIn>
-                <div className="flex h-screen w-screen flex-row overflow-y-hidden bg-stone-300">
-                    <SideNav />
-                    <main className="flex w-full flex-1 flex-col">
-                        <div className="flex w-full grow flex-col rounded-tl-lg bg-stone-200 px-4">
-                            {children}
-                        </div>
-                    </main>
+        <div className="flex h-screen w-screen flex-row overflow-y-hidden bg-stone-300">
+            <SideNav />
+            <main className="flex w-full flex-1 flex-col">
+                <div className="flex w-full grow flex-col rounded-tl-lg bg-stone-200 px-4">
+                    {children}
                 </div>
-            </SignedIn>
-        </>
+            </main>
+        </div>
     );
 }
--- src/app/[server_id]/layout.tsx
+++ src/app/[server_id]/layout.tsx
@@ -1,8 +1,20 @@
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+
 interface Props {
     children: React.ReactNode;
 }
 
 export default function ServerLayout({ children }: Props) {
     console.log("server layout");
-    return <>{children}</>;
+
+    return (
+        <>
+            <SignedOut>
+                <h1>You are not logged in</h1>
+                <p>Please log in to continue</p>
+                <SignInButton />
+            </SignedOut>
+            <SignedIn>{children}</SignedIn>
+        </>
+    );
 }
