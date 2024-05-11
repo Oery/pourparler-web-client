dd049efb7fa988dac65fc0bd8bc4fe47134d239a
Oery <48154940+Oery@users.noreply.github.com>
2024-05-11 16:09:03 +0200

    Add User Typing

--- src/app/[channel_id]/_components/chat-input.tsx
+++ src/app/[channel_id]/_components/chat-input.tsx
@@ -3,8 +3,21 @@
 import { useCallback, useEffect, useState } from "react";
 import { useSocket } from "../_hooks/use-socket";
 
+function getTypingString(usersTyping: string[]) {
+    switch (usersTyping.length) {
+        case 1:
+            return `${usersTyping[0]} is typing...`;
+        case 2:
+            return `${usersTyping[0]} and ${usersTyping[1]} are typing...`;
+        default:
+            return "";
+    }
+}
 export default function ChatInput() {
     const [message, setMessage] = useState("");
+    const [usersTyping, setUsersTyping] = useState<string[]>([]);
+    const [wasTyping, setWasTyping] = useState(false);
+    const [typingString, setTypingString] = useState("");
 
     const socket = useSocket();
 
@@ -32,13 +45,71 @@ export default function ChatInput() {
         [message, socket],
     );
 
+    const handleUserStartTyping = useCallback(
+        ({ username }: { username: string }) => {
+            if (usersTyping.includes(username)) return;
+            setUsersTyping((prev) => [...prev, username]);
+        },
+        [usersTyping],
+    );
+
+    const handleUserStopTyping = useCallback(
+        ({ username }: { username: string }) => {
+            setUsersTyping((prev) => prev.filter((user) => user !== username));
+        },
+        [],
+    );
+
+    useEffect(() => {
+        const newTypingString = getTypingString(usersTyping);
+        if (newTypingString.length > 0) {
+            setTypingString(newTypingString);
+        }
+    }, [usersTyping]);
+
+    useEffect(() => {
+        if (!socket) return;
+
+        socket.on("typing:start", handleUserStartTyping);
+        socket.on("typing:stop", handleUserStopTyping);
+
+        return () => {
+            socket?.off("typing:start");
+            socket?.off("typing:stop");
+        };
+    }, [handleUserStartTyping, handleUserStopTyping, socket]);
+
+    useEffect(() => {
+        if (!socket) return;
+        const isTyping = message.length > 0;
+
+        if (isTyping && !wasTyping) {
+            socket.emit("typing:start", {
+                channel: 1,
+            });
+            setWasTyping(true);
+        } else if (!isTyping && wasTyping) {
+            socket.emit("typing:stop", {
+                channel: 1,
+            });
+            setWasTyping(false);
+        }
+    }, [message, socket, wasTyping]);
+
     return (
         <form
-            className="shrink-0 grow-0 basis-auto pb-3"
+            className="relative shrink-0 grow-0 basis-auto pb-3"
             onSubmit={handleSubmit}
         >
+            <div
+                className={`absolute bottom-14 z-0 w-full transition-transform ${usersTyping.length > 0 ? "translate-y-0" : "translate-y-10"}`}
+            >
+                <p className="h-7 w-full rounded-md bg-stone-200 px-2 text-sm text-stone-600">
+                    {typingString}
+                </p>
+            </div>
             <input
-                className="w-full rounded-lg border-none p-3 outline-none"
+                className="relative z-10 w-full rounded-lg border-none p-3 outline-none"
                 type="text"
                 placeholder="Type a message..."
                 value={message}
