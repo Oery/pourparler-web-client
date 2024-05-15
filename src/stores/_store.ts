2f3c7ef2727f11265afd583f9b92ee15f3452cb3
Oery <48154940+Oery@users.noreply.github.com>
2024-05-15 16:31:05 +0200

    Removed Channel Store

--- src/stores/_store.ts
+++ src/stores/_store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { messageSlice } from "./messages";
-import { channelSlice } from "./channels";
 import { memberSlice } from "./members";
 
 export const makeStore = () =>
     configureStore({
         reducer: {
             messages: messageSlice.reducer,
-            channels: channelSlice.reducer,
             members: memberSlice.reducer,
         },
     });
