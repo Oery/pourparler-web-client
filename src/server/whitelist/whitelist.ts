90e9f556b34a89033120e4249aed1ff5f4e315db
Oery <48154940+Oery@users.noreply.github.com>
2024-05-16 12:06:02 +0200

    Fix Whitelist

--- src/server/whitelist/whitelist.ts
+++ src/server/whitelist/whitelist.ts
@@ -1,5 +1,5 @@
 import whitelist from "./whitelist.json";
 
 export function isWhitelisted(userId: string) {
-    return Object.values(whitelist).includes(userId);
+    return whitelist.includes(userId);
 }
