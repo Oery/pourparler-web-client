6969db221882af74847b5f55f94df88c8678d77d
Oery <48154940+Oery@users.noreply.github.com>
2024-05-16 12:02:01 +0200

    Add Server Loading into state

--- src/app/[server_id]/_components/pourparler-client.tsx
+++ src/app/[server_id]/_components/pourparler-client.tsx
@@ -2,28 +2,40 @@
 
 import { useRef } from "react";
 import { Provider } from "react-redux";
-import type { ServerWithChannels } from "~/app/_types/server";
+import type { MessageWithAuthor } from "~/app/_types/message";
+import type { Server } from "~/app/_types/server";
 import { makeStore, type AppStore } from "~/stores/_store";
 import { setMessages } from "~/stores/messages";
+import { setServers } from "~/stores/servers";
 import { serializeMessage } from "~/utils/serialize";
 
 interface Props {
     children: React.ReactNode;
-    server: ServerWithChannels;
+    server: Server;
 }
 
 export default function PourparlerClient({ children, server }: Props) {
     const storeRef = useRef<AppStore | null>(null);
 
-    const messages = server.channels.flatMap((channel) => channel.messages);
+    const messages = server.channels.flatMap((channel) => channel?.messages);
 
     if (!storeRef.current) {
         storeRef.current = makeStore();
 
-        const serializedMessages = messages.map((message) =>
+        const serializedMessages = messages.map((message: MessageWithAuthor) =>
             serializeMessage(message),
         );
+
+        // Remove messages from channels
+        const serverWithoutMessages: Server = {
+            ...server,
+            channels: server.channels.map((channel) => {
+                return { ...channel, messages: [] };
+            }),
+        };
+
         storeRef.current.dispatch(setMessages(serializedMessages));
+        storeRef.current.dispatch(setServers([serverWithoutMessages]));
     }
 
     return <Provider store={storeRef.current}>{children}</Provider>;
