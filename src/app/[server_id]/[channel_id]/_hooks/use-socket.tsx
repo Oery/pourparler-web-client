cc0f5294e701670439a9c6dd1abd8f3a6a6dcee7
Oery <48154940+Oery@users.noreply.github.com>
2024-05-16 11:55:45 +0200

    Add Channel Event Handling

--- src/app/[server_id]/[channel_id]/_hooks/use-socket.tsx
+++ src/app/[server_id]/[channel_id]/_hooks/use-socket.tsx
@@ -1,7 +1,16 @@
 "use client";
 
-import { createContext, useContext, useMemo } from "react";
+import {
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useMemo,
+} from "react";
+import { useDispatch } from "react-redux";
 import { io, type Socket } from "socket.io-client";
+import type { Channel, ChannelDelete } from "~/app/_types/channel";
+import { addChannel, removeChannel } from "~/stores/servers";
 
 const SocketContext = createContext<Socket | null>(null);
 
@@ -12,6 +21,32 @@ export const useSocket = () => {
 
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
     const socket = useMemo(() => io("127.0.0.1:8000"), []);
+    const dispatch = useDispatch();
+
+    const handleChannelDelete = useCallback(
+        (data: ChannelDelete) => {
+            console.log("received delete event:", data);
+            dispatch(removeChannel(data));
+        },
+        [dispatch],
+    );
+
+    const handleChannelCreate = useCallback(
+        (data: Channel) => {
+            dispatch(addChannel(data));
+        },
+        [dispatch],
+    );
+
+    useEffect(() => {
+        socket?.on("channel:create", handleChannelCreate);
+        socket?.on("channel:delete", handleChannelDelete);
+
+        return () => {
+            socket?.off("channel:create");
+            socket?.off("channel:delete");
+        };
+    }, [handleChannelCreate, handleChannelDelete, socket]);
 
     return (
         <SocketContext.Provider value={socket}>
