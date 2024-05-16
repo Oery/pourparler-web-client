a1b96ca6c937c7f27386f0841b5d17b65c4ea0d4
Oery <48154940+Oery@users.noreply.github.com>
2024-05-16 12:00:56 +0200

    Removed unnecessary DB Call

--- src/app/[server_id]/[channel_id]/page.tsx
+++ src/app/[server_id]/[channel_id]/page.tsx
@@ -1,21 +1,20 @@
-import { db } from "~/server/db";
+"use client";
+
 import ChannelBar from "./_components/channel-bar";
 import Chat from "./_components/chat";
-
-export const dynamic = "force-dynamic";
+import { useSelector } from "react-redux";
+import { serversSelector } from "~/stores/servers";
 
 interface Props {
-    params: { channel_id: string };
+    params: { channel_id: string; server_id: string };
 }
 
-export default async function ChannelPage({ params }: Props) {
-    const channel = await db.query.channels.findFirst({
-        where: (channels, { eq }) => eq(channels.id, params.channel_id),
-    });
+export default function ChannelPage({ params }: Props) {
+    const channel = useSelector(serversSelector)
+        .find((server) => server.id === params.server_id)
+        ?.channels.find((channel) => channel.id === params.channel_id);
 
-    if (!channel) {
-        return <h1>Channel not found</h1>;
-    }
+    if (!channel) return <h1>Channel not found</h1>;
 
     return (
         <>
