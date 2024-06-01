e8b103243917001148d92d4acf91c20f722953de
Oery <48154940+Oery@users.noreply.github.com>
2024-06-01 22:30:13 +0200

    Add Channel Context Menu Admin Check

--- src/app/components/app/channel.tsx
+++ src/app/components/app/channel.tsx
@@ -5,6 +5,10 @@ import { type Channel } from "~/app/_types/channel";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import ChannelContextMenu from "~/app/components/context-menus/channel-cm";
+import { useSelector } from "react-redux";
+import { serversSelector } from "~/stores/servers";
+import { useAuth } from "@clerk/nextjs";
+import type { Server } from "~/app/_types/server";
 
 interface Props {
     channel: Channel;
@@ -13,10 +17,17 @@ interface Props {
 export default function ChannelComponent({ channel }: Props) {
     const pathname = usePathname();
     const isActive = pathname === `/${channel.id}`;
+    const { userId } = useAuth();
+
+    const serverOwner = useSelector<string, Server[]>(serversSelector).find(
+        (server) => server.id === channel.serverId,
+    )?.ownerId;
+
+    const isAdmin = userId === serverOwner;
 
     return (
         <Link href={`./${channel.id}`} className="flex justify-between">
-            <ChannelContextMenu channel={channel}>
+            <ChannelContextMenu channel={channel} isAdmin={isAdmin}>
                 <div
                     className={`${isActive && "bg-stone-200"} flex cursor-pointer items-center gap-3 truncate rounded-md px-4 py-1 text-base font-light transition-all hover:translate-x-2 hover:bg-stone-200`}
                 >
--- src/app/components/context-menus/channel-cm.tsx
+++ src/app/components/context-menus/channel-cm.tsx
@@ -11,9 +11,10 @@ import type { Channel } from "~/app/_types/channel";
 interface Props {
     children: React.ReactNode;
     channel: Channel;
+    isAdmin: boolean;
 }
 
-export default function ChannelContextMenu({ children, channel }: Props) {
+function ChannelContextMenu({ children, channel, isAdmin }: Props) {
     const handleDelete = useCallback(async () => {
         const formData = new FormData();
         formData.append("serverId", channel.serverId);
@@ -26,10 +27,15 @@ export default function ChannelContextMenu({ children, channel }: Props) {
             <ContextMenuTrigger>{children}</ContextMenuTrigger>
 
             <ContextMenuContent>
-                <ContextMenuItem onClick={handleDelete}>
-                    Delete Channel
-                </ContextMenuItem>
+                <ContextMenuItem>This is a context menu</ContextMenuItem>
+                {isAdmin && (
+                    <ContextMenuItem onClick={handleDelete}>
+                        Delete Channel
+                    </ContextMenuItem>
+                )}
             </ContextMenuContent>
         </ContextMenu>
     );
 }
+
+export default ChannelContextMenu;
