979ec5749652cda73b1daf4f79f4351477877ae2
Oery <48154940+Oery@users.noreply.github.com>
2024-06-01 19:50:43 +0200

    Add Error Handling

--- src/app/[server_id]/layout.tsx
+++ src/app/[server_id]/layout.tsx
@@ -2,29 +2,38 @@ import { auth } from "@clerk/nextjs/server";
 import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 import { db } from "~/server/db";
 import PourparlerClient from "~/app/components/app/pourparler-client";
+import type { Server } from "../_types/server";
 
 interface Props {
     children: React.ReactNode;
+    params: { server_id: string };
 }
 
-export default async function ServerLayout({ children }: Props) {
+export default async function ServerLayout({ children, params }: Props) {
     auth().protect();
+    let server: Server | undefined = undefined;
 
-    const server = await db.query.servers.findFirst({
-        columns: { createdAt: false },
-        with: {
-            channels: {
-                columns: { createdAt: false },
-                with: {
-                    messages: {
-                        columns: { createdAt: false, updatedAt: false },
-                        with: { author: { columns: { createdAt: false } } },
+    try {
+        server = await db.query.servers.findFirst({
+            columns: { createdAt: false },
+            with: {
+                channels: {
+                    columns: { createdAt: false },
+                    with: {
+                        messages: {
+                            columns: { createdAt: false, updatedAt: false },
+                            with: { author: { columns: { createdAt: false } } },
+                        },
                     },
                 },
+                categories: { columns: { createdAt: false } },
             },
-            categories: { columns: { createdAt: false } },
-        },
-    });
+            where: (servers, { eq }) => eq(servers.id, params.server_id),
+        });
+    } catch (err) {
+        console.error("Error fetching server", err);
+        return <h1>Server not found</h1>;
+    }
 
     if (!server) return <h1>Server not found</h1>;
 
