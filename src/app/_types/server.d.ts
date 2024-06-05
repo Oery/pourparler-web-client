7c1d9c22cf66fc27fa158b56dbed5d34d218a33c
Oery <48154940+Oery@users.noreply.github.com>
2024-06-05 16:26:41 +0200

    Removed Channels Property

--- src/app/_types/server.d.ts
+++ src/app/_types/server.d.ts
@@ -1,10 +1,8 @@
-import type { Channel } from "./channel";
 import type { Category } from "./category";
 
 export interface Server {
     id: string;
     name: string;
-    channels: Channel[];
     categories: Category[];
     ownerId: string;
 }
