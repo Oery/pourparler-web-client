8a700a170ceb0767557fd8dbc4db203938361994
Oery <48154940+Oery@users.noreply.github.com>
2024-06-05 16:37:54 +0200

    Migrate to Channel store

--- src/app/[server_id]/[channel_id]/page.tsx
+++ src/app/[server_id]/[channel_id]/page.tsx
@@ -3,18 +3,16 @@
 import ChannelBar from "~/app/components/app/channel-bar";
 import Chat from "~/app/components/chat/chat";
 import { useSelector } from "react-redux";
-import { serversSelector } from "~/stores/servers";
-import type { Server } from "~/app/_types/server";
-import type { RootState } from "~/stores/_store";
+import { channelsSelector } from "~/stores/channels";
 
 interface Props {
     params: { channel_id: string; server_id: string };
 }
 
 export default function ChannelPage({ params }: Props) {
-    const channel = useSelector<RootState, Server[]>(serversSelector)
-        .find((server) => server.id === params.server_id)
-        ?.channels.find((channel) => channel.id === params.channel_id);
+    const channel = useSelector(channelsSelector).find(
+        (channel) => channel.id === params.channel_id,
+    );
 
     if (!channel) return <h1>Channel not found</h1>;
 
